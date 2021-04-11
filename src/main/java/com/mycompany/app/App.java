f9bd4cebb866cb0f1f1f23dc3dd9cd847d965811
akshayugale211 <72296137+akshayugale211@users.noreply.github.com>
2021-04-11 13:29:14 +0530

    Update App.java
    Changed output message

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!";
+    private final String message = "Hello World! \nThis is Akshay Ugale (1721515)";
 
     public App() {}
 
